b9385846a0afbf255f980fd430270d65b0981fc1
Takara Satone <takarasatone@gmail.com>
2016-11-15 19:02:31 -0800

    made reviews model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110000707) do
+ActiveRecord::Schema.define(version: 20161116021849) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
